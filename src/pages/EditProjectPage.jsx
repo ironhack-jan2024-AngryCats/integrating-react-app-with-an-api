168268d827e13f22a9ac2cb9e9df48b472d99eac
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-02-13 12:46:11 +0100

    add functionality to edit projects

---
+++ src/pages/EditProjectPage.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+function EditProjectPage() {
+
+    const API_URL = "https://project-management-api-4641927fee65.herokuapp.com";
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const { projectId } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`${API_URL}/projects/${projectId}`)
+            .then((response) => {
+                setTitle(response.data.title);
+                setDescription(response.data.description)
+            })
+            .catch((error) => {
+                console.log("Error getting project details from the API...");
+                console.log(error);
+            });
+    }, [projectId]);
+
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const newDetails = {
+            title: title,
+            description: description
+        }
+
+        axios.put(`${API_URL}/projects/${projectId}`, newDetails)
+            .then((response) => {
+                navigate(`/projects/${projectId}`);
+            })
+            .catch((error) => {
+                console.log("Error updating project...");
+                console.log(error);
+            });
+    }
+
+
+    return (
+        <div className="EditProjectPage">
+            <h3>Edit the Project</h3>
+
+            <form onSubmit={handleFormSubmit}>
+                <label>Title:</label>
+                <input
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Description:</label>
+                <textarea
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+
+                <button type="submit">Update Project</button>
+            </form>
+        </div>
+    );
+}
+
+export default EditProjectPage;
